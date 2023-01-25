86f021151635cb66ad83b288f79edb2e307930e8
jishan ahmmed <122050293+jishanAhamed@users.noreply.github.com>
2023-01-26 05:00:43 +0600

    Add files via upload

---
+++ js/my.js
@@ -0,0 +1,7 @@
+var btn = document.getElementById("btn")
+var lightimg = document.getElementById("lightimg")
+
+function togglebtn(){
+    btn.classList.toggle("active")
+    lightimg.classList.toggle("light-on")
+}
